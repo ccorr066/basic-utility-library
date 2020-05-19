0b4dcd03ff5742426bead415c32e1a2c653b52fc
ccorr066@fiu.edu <punchcode@Punchcodes-MacBook-Pro.local>
2020-05-18 22:58:53 -0700

    added two more functions

--- functions.js
+++ functions.js
@@ -1,22 +1,42 @@
-function add(input1, input2) {
-   // A1: any JavaScript value
-   // A2: any JavaScript value
-   // R: a single JavaScript value
-   return input1 + input2;
+function glueStringsPlusEquals(string1, string2) {
+   // A1: a set of words
+   // A2: a set of words
+   // R: a sentence from A1 and A2 combine with each other
+   return (string1 += string2);
 }
-
-function difference(input1, input2) {
-   // A1: a number, not letter or any other special characters
-   // A2: a number, not letter or any other special characters
+function glueStrings(string1, string2) {
+   // A1: a set of words
+   // A2: a set of words
+   // R: a sentence from A1 and A2 combine with each other
+   return string1 + string2;
+}
+function quotientDecimals(input1, input2) {
+   // A1: a number with decimals, not letter or any other special characters
+   // A2: a number with decimals, not letter or any other special characters
    // R: a value from A1 and A2
-   return input1 - input2;
+   return input1 / input2;
 }
 
-function product(input1, input2) {
-   // A1: a number, not letter or any other special characters
-   // A2: a number, not letter or any other special characters
+function productDecimals(input1, input2) {
+   // A1: a number with decimals, not letter or any other special characters
+   // A2: a number with decimals, not letter or any other special characters
    // R: a value from A1 and A2
    return input1 * input2;
+}
+
+function decrement(input1) {
+   // A1: a number, not letter or any other special characters
+   // A2: deducts one less from the return
+   // R: a value from A1 and A2
+   input1--;
+   return input1;
+}
+function increment(input1) {
+   // A1: a number, not letter or any other special characters
+   // A2: adds one more to the return
+   // R: a value from A1 and A2
+   input1++;
+   return input1;
 }
 
 function quotient(input1, input2) {
@@ -26,30 +46,23 @@ function quotient(input1, input2) {
    return input1 / input2;
 }
 
-function increment(input1) {
+function product(input1, input2) {
    // A1: a number, not letter or any other special characters
-   // A2: adds one more to the return
-   // R: a value from A1 and A2
-   input1++;
-   return input1;
-}
-function decrement(input1) {
-   // A1: a number, not letter or any other special characters
-   // A2: deducts one less from the return
-   // R: a value from A1 and A2
-   input1--;
-   return input1;
-}
-
-function productDecimals(input1, input2) {
-   // A1: a number with decimals, not letter or any other special characters
-   // A2: a number with decimals, not letter or any other special characters
+   // A2: a number, not letter or any other special characters
    // R: a value from A1 and A2
    return input1 * input2;
 }
-function quotientDecimals(input1, input2) {
-   // A1: a number with decimals, not letter or any other special characters
-   // A2: a number with decimals, not letter or any other special characters
+
+function difference(input1, input2) {
+   // A1: a number, not letter or any other special characters
+   // A2: a number, not letter or any other special characters
    // R: a value from A1 and A2
-   return input1 / input2;
+   return input1 - input2;
+}
+
+function add(input1, input2) {
+   // A1: any JavaScript value
+   // A2: any JavaScript value
+   // R: a single JavaScript value
+   return input1 + input2;
 }
