21f93d741313d985204a820e0f05e0e3a94bfd0f
ccorr066@fiu.edu <punchcode@Punchcodes-MacBook-Pro.local>
2020-05-18 22:25:15 -0700

    added surge and 8 functions

--- functions.js
+++ functions.js
@@ -4,3 +4,52 @@ function add(input1, input2) {
    // R: a single JavaScript value
    return input1 + input2;
 }
+
+function difference(input1, input2) {
+   // A1: a number, not letter or any other special characters
+   // A2: a number, not letter or any other special characters
+   // R: a value from A1 and A2
+   return input1 - input2;
+}
+
+function product(input1, input2) {
+   // A1: a number, not letter or any other special characters
+   // A2: a number, not letter or any other special characters
+   // R: a value from A1 and A2
+   return input1 * input2;
+}
+
+function quotient(input1, input2) {
+   // A1: a number, not letter or any other special characters
+   // A2: a number, not letter or any other special characters
+   // R: a value from A1 and A2
+   return input1 / input2;
+}
+
+function increment(input1) {
+   // A1: a number, not letter or any other special characters
+   // A2: adds one more to the return
+   // R: a value from A1 and A2
+   input1++;
+   return input1;
+}
+function decrement(input1) {
+   // A1: a number, not letter or any other special characters
+   // A2: deducts one less from the return
+   // R: a value from A1 and A2
+   input1--;
+   return input1;
+}
+
+function productDecimals(input1, input2) {
+   // A1: a number with decimals, not letter or any other special characters
+   // A2: a number with decimals, not letter or any other special characters
+   // R: a value from A1 and A2
+   return input1 * input2;
+}
+function quotientDecimals(input1, input2) {
+   // A1: a number with decimals, not letter or any other special characters
+   // A2: a number with decimals, not letter or any other special characters
+   // R: a value from A1 and A2
+   return input1 / input2;
+}
