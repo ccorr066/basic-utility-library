1155c877779645b588c24c19744cb7be197e733a
ccorr066@fiu.edu <punchcode@Punchcodes-MacBook-Pro.local>
2020-05-20 00:03:54 -0700

    added 7 functions

--- functions.js
+++ functions.js
@@ -1,8 +1,76 @@
+function standLine(string1, string2) {
+   // A1:
+   // A2:
+   // R:
+   return string1;
+}
+function shoppingList(string1, string2) {
+   // A1:
+   // A2:
+   // R:
+   return string1;
+}
+function arrWithUnshift(string1, string2) {
+   // A1:
+   // A2:
+   // R:
+   return string1;
+}
+function arrWithShift(string1, string2) {
+   // A1:
+   // A2:
+   // R:
+   return string1([]);
+}
+function arrWithPop(string1, string2) {
+   // A1:
+   // A2:
+   // R:
+   return string1.pop([]);
+}
+function arrWithPush(string1) {
+   // A1:
+   // R:
+   return string1.push([]);
+}
+
+function nLastCharaString(string1) {
+   // A1: a set of characters
+   // R: the second to last letter of the word
+   return string1[string1.length - 2];
+}
+function lastCharaString(string1) {
+   // A1: a set of characters
+   // R: the last letter of the word
+   return string1[string1.length - 1];
+}
+
+function nCharaString(string1) {
+   // A1: user types a set of characters
+   // R: picks the 4 character of the string. (remember the computer starts from 0)
+   return string1[3];
+}
+function firstCharaString(string1) {
+   // A1: user types a set of characters
+   // R: the first letter of the word
+   return string1[0];
+}
+
+function lengthString(string1) {
+   // A1: user inputs word
+   // R: count the amount of characters in a word
+   return string1.length;
+}
+function stringsWithVariables(string1) {
+   // A1: user inputs in favorite sport
+   // R: user inputs combined with a preset set of strings
+   return "Do you love " + string1 + " like the professionals?";
+}
 function glueStringsPlusEquals(string1, string2) {
    // A1: a set of words
    // A2: a set of words
    // R: a sentence from A1 and A2 combine with each other
-   return (string1 += string2);
+   return (string1 += " " + string2);
 }
 function glueStrings(string1, string2) {
    // A1: a set of words
